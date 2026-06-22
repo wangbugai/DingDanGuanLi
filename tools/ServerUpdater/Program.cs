8dfd73619a4e9581e36ac3530a0628791559fc9a
wang-chaoran2 <648971587@qq.com>
2026-06-22 17:18:47 +0800

    fix: 注册GBK编码提供程序解决.NET不支持gbk的问题

--- tools/ServerUpdater/Program.cs
+++ tools/ServerUpdater/Program.cs
@@ -10,6 +10,7 @@ internal static class Program
     [STAThread]
     private static void Main()
     {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         ApplicationConfiguration.Initialize();
         Application.Run(new MainForm());
     }
